ac86d7cb646be963fa23a5246831aa494132c5cd
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-03-13 18:10:50 +0700

    update src/main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import plugin from '../plugin.json';
 import styles from "./styles.scss";
-// import acode from "acode";
+import acode from "acode";
 
 const fs = acode.require("fs");
 const settings = acode.require("settings");
